19cb7c4d3cb2d85099a9e08b9896bf1374671b0c
Boi <cambodianjesuss@gmail.com>
2023-10-29 00:25:11 -0700

    Implemented a very basic event bus to recieve an event and broadcast to the other services

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.post('/events', (req,res)=>{
+  const events = req.body;
+
+  axios.post('http://localhost:4000/events', events).catch(error=>{
+    console.log(error.message);
+  });
+  axios.post('http://localhost:4001/events', events).catch(error=>{
+    console.log(error.message);
+  });
+  axios.post('http://localhost:4002/events', events).catch(error=>{
+    console.log(error.message);
+  });
+
+  res.send({ status: "OK"});
+});
+
+app.listen(4005, ()=>{
+  console.log('Event Bus on 4005')
+});
