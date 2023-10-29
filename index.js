e001cf19ae1f5df83e59e1e613bebad71b6ca74c
Boi <cambodianjesuss@gmail.com>
2023-10-29 14:08:08 -0700

    testing emmiting events and recieving in event bus

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/events', (req,res)=>{
   const events = req.body;
 
-  console.log(events)
+  console.log(`Event recieved: ${Object.values(events)}`);
 
-  axios.post('http://localhost:4000/events', events).catch(error=>{
-    console.log(error.message);
-  });
-  axios.post('http://localhost:4001/events', events).catch(error=>{
-    console.log(error.message);
-  });
-  axios.post('http://localhost:4002/events', events).catch(error=>{
-    console.log(error.message);
-  });
+
+  // axios.post('http://localhost:4000/events', events).catch(error=>{
+  //   console.log(error.message);
+  // });
+  // axios.post('http://localhost:4001/events', events).catch(error=>{
+  //   console.log(error.message);
+  // });
+  // axios.post('http://localhost:4002/events', events).catch(error=>{
+  //   console.log(error.message);
+  // });
 
   res.send({ status: "OK"});
 });
