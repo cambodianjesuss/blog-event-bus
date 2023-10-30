7f41ff15bba03e9d7c661cd3663647078f283b19
Boi <cambodianjesuss@gmail.com>
2023-10-30 14:14:28 -0700

    query service emit

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ app.post('/events', (req,res)=>{
   axios.post('http://localhost:4001/events', events).catch(error=>{
     console.log(error.message);
   });
-  // axios.post('http://localhost:4002/events', events).catch(error=>{
-  //   console.log(error.message);
-  // });
+  //Query Service Events
+  axios.post('http://localhost:4002/events', events).catch(error=>{
+    console.log(error.message);
+  });
 
   res.send({ status: "OK"});
 });
