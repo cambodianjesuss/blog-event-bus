b5ce53407754c10825caca53de3d69c776e7ca9f
Boi <cambodianjesuss@gmail.com>
2023-11-06 00:20:55 -0800

    update

--- index.js
+++ index.js
@@ -20,15 +20,14 @@ app.post('/events', (req,res)=>{
   axios.post('http://localhost:4001/events', events).catch(error=>{
     console.log(error.message);
   });
+   //Query Service Events
+   axios.post('http://localhost:4002/events', events).catch(error=>{
+    console.log(error.message);
+  });
   // Moderation Service Events
   axios.post('http://localhost:4003/events', events).catch(error => {
     console.log(error.message);
   });
-  //Query Service Events
-  axios.post('http://localhost:4002/events', events).catch(error=>{
-    console.log(error.message);
-  });
-
   res.send({ status: "OK"});
 });
 
